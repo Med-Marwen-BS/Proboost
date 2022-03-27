ca73908d562d52da8ef6dcda6403b02aa6d7ec9a
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2022-03-27 15:25:29 +0100

    add Regex Email

--- src/main/java/com/proboost/proboostproject/registration/EmailValidator.java
+++ src/main/java/com/proboost/proboostproject/registration/EmailValidator.java
@@ -3,12 +3,19 @@ package com.proboost.proboostproject.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
 //        TODO: Regex to validate email
-        return true;
+     /**   String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+
+        return matcher.matches();*/
+        return true ;
     }
 }
