a57ecb9fa8a4d2fff65e22371360f38bfc2a18fb
Amira <benkridamira14@gmail.com>
2022-03-29 14:08:31 +0200

    offre V1

--- src/main/java/com/proboost/proboostproject/Services/OffreService.java
+++ src/main/java/com/proboost/proboostproject/Services/OffreService.java
@@ -5,6 +5,8 @@ import com.proboost.proboostproject.Respositories.OffreRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,6 +17,7 @@ public class OffreService {
 
     public Offre_Emploi add(Offre_Emploi offreEmploi)
     {
+        offreEmploi.setPostedDate(LocalDateTime.now());
         return offreRepo.save(offreEmploi);
     }
 
@@ -23,10 +26,28 @@ public class OffreService {
         return offreRepo.findAll();
     }
 
+    public Offre_Emploi getOffre( int id) {
+        return offreRepo.getById(id);
+    }
+
     public String delete(int id)
     {
 
         offreRepo.delete(offreRepo.findById(id).get());
         return "Offre Supprimée";
     }
+
+ /*   public  Offre_Emploi update(Offre_Emploi newoffre )
+    {
+        return offreRepo.save(newoffre);
+    }*/
+
+
+    public Offre_Emploi update(Offre_Emploi offreEmploi)
+    {
+//        offreEmploi.setPostedDate(LocalDate.now());
+        return offreRepo.save(offreEmploi);
+    }
+
+
 }
