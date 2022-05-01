b210aaa8a01afa3fd27682a9b87b0fcc8bbf69fd
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2022-05-01 03:51:25 +0100

    cors correction

--- src/main/java/com/proboost/proboostproject/security/config/WebSecurityConfig.java
+++ src/main/java/com/proboost/proboostproject/security/config/WebSecurityConfig.java
@@ -33,7 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        //http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
       http.authorizeRequests().antMatchers("/login","/api/users/refresh_token","/api/v1/**").permitAll() ;
